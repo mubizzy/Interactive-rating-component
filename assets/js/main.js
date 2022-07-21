e48e2ee5e509c5b76709356063e8121bfad68cac
unknown <ajibolamubarak304@gmail.com>
2022-07-21 05:34:06 +0100

    working on the second section

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,8 @@ const submitButton = document.getElementById("submit-rating");
 const rateAgain = document.getElementById("rate-again");
 const ratings = document.querySelectorAll(".btn");
 const actualRating = document.getElementById("rating");
+
+submitButton.addEventListener("click", () => {
+  mainContainer.style.display = "none";
+  thanksContainer.classList.remove("hidden");
+});
