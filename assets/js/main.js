247a875170d137174638c5a250ade91ce8c63338
unknown <ajibolamubarak304@gmail.com>
2022-07-21 06:00:58 +0100

    working on the second section

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,4 +8,14 @@ const actualRating = document.getElementById("rating");
 submitButton.addEventListener("click", () => {
   mainContainer.style.display = "none";
   thanksContainer.classList.add("show-menu");
+  ratings.forEach((rating) => {
+    rating.addEventListener("click", () => {
+      actualRating.innerHTML = rating.innerHTML;
+    });
+  });
+});
+
+rateAgain.addEventListener("click", () => {
+  mainContainer.style.display = "block";
+  thanksContainer.classList.remove("show-menu");
 });
