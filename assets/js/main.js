ddc628ced471660aea167b23bc96c0a50989f760
unknown <ajibolamubarak304@gmail.com>
2022-07-21 05:28:07 +0100

    working on the second section

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+const mainContainer = document.querySelector(".container");
+const thanksContainer = document.querySelector(".thank-you__container");
+const submitButton = document.getElementById("submit-rating");
+const rateAgain = document.getElementById("rate-again");
+const ratings = document.querySelectorAll(".btn");
+const actualRating = document.getElementById("rating");
